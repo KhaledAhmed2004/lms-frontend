9dfb7f9f2dbc1abd2c3415486aff305735bba636
Pronoy <pronoypaul56@gmail.com>
2026-04-07 22:46:39 +0600

    border line fixed in student dashboard message

--- src/app/[locale]/(student)/student/messages/page.tsx
+++ src/app/[locale]/(student)/student/messages/page.tsx
@@ -4,7 +4,7 @@ import Messages from "@/components/messages/Messages";
 
 export default function MessagesPage() {
   return (
-    <div className="h-full  border border-border">
+    <div className="h-full  border rounded-xl">
       <Messages />
     </div>
   );
